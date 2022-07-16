8bfe95ce76058d73599fbf5bcd92008788b9e77f
tharun ganapathi <65713157+tharunganapathi@users.noreply.github.com>
2022-07-17 01:09:35 +0530

    Update Open_crash_script.py

--- Open_crash_script.py
+++ Open_crash_script.py
@@ -1,5 +1,11 @@
 def test():
+    nuke.scriptClear()
     nuke.scriptOpen("")
+    #nuke.message('hi')
+    for i in nuke.allNodes():
+        i.setSelected(True)
+        nukescripts.toggle("disable")
+        i.setSelected(False)
 
 
 nuke.menu('Nuke').addMenu('Tharun').addCommand('openscript',test,'Shuffle.png')
